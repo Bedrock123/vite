e0035d727b6a647e1393f65edfa16f64dee8c6bd
ZB <zachary@cleancult.com>
2024-03-28 18:52:26 -0400

    added

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,20 @@
 import NavButton from "../components/NavButton";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { AppShell } from "@mantine/core";
+import "@mantine/charts/styles.css";
 
+import React, { Suspense } from "react";
+const TanStackRouterDevtoolsLazy =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        }))
+      );
 export const Route = createRootRoute({
   component: () => (
     <>
@@ -20,15 +32,15 @@ export const Route = createRootRoute({
         </AppShell.Header>
 
         <AppShell.Navbar p="md">
-          {" "}
           <NavButton to="/">Home</NavButton>
           <NavButton to="/about">About</NavButton>
         </AppShell.Navbar>
 
         <AppShell.Main>
-          {" "}
           <Outlet />
-          <TanStackRouterDevtools />
+          <Suspense>
+            <TanStackRouterDevtoolsLazy />
+          </Suspense>
         </AppShell.Main>
       </AppShell>
     </>
