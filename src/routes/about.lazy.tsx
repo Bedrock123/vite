f3007cbcda3b1d900a54056f0abe1676563216bf
ZB <zachary@cleancult.com>
2024-03-29 10:14:15 -0400

    added

--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -46,36 +46,6 @@ function About() {
     },
   };
 
-  const config2 = {
-    data: {
-      type: "fetch",
-      value: "https://assets.antv.antgroup.com/g2/energy.json",
-    },
-    scale: {
-      color: {
-        range: [
-          "#4e79a7",
-          "#f28e2c",
-          "#e15759",
-          "#76b7b2",
-          "#59a14f",
-          "#edc949",
-          "#af7aa1",
-          "#ff9da7",
-          "#9c755f",
-          "#bab0ab",
-        ],
-      },
-    },
-    layout: { nodeAlign: "center", nodePadding: 0.03 },
-    style: {
-      labelSpacing: 3,
-      labelFontWeight: "bold",
-      nodeStrokeWidth: 1.2,
-      linkFillOpacity: 0.4,
-    },
-  };
-
   const config3 = {
     data: {
       type: "fetch",
