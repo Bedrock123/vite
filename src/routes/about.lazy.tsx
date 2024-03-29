51f694701edbf589ada393fe3bb62c7499aaf1fc
ZB <zachary@cleancult.com>
2024-03-29 10:14:00 -0400

    Added

--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -1,7 +1,9 @@
 import { Outlet, createLazyFileRoute } from "@tanstack/react-router";
 import NavButton from "../components/NavButton";
 import { Area } from "@ant-design/plots";
-import { Box } from "@mantine/core";
+import { Box, Grid } from "@mantine/core";
+import { Sankey } from "@ant-design/plots";
+import { Column } from "@ant-design/plots";
 
 export const Route = createLazyFileRoute("/about")({
   component: About,
@@ -45,11 +47,74 @@ function About() {
     },
   };
 
+  const config2 = {
+    data: {
+      type: "fetch",
+      value: "https://assets.antv.antgroup.com/g2/energy.json",
+    },
+    scale: {
+      color: {
+        range: [
+          "#4e79a7",
+          "#f28e2c",
+          "#e15759",
+          "#76b7b2",
+          "#59a14f",
+          "#edc949",
+          "#af7aa1",
+          "#ff9da7",
+          "#9c755f",
+          "#bab0ab",
+        ],
+      },
+    },
+    layout: { nodeAlign: "center", nodePadding: 0.03 },
+    style: {
+      labelSpacing: 3,
+      labelFontWeight: "bold",
+      nodeStrokeWidth: 1.2,
+      linkFillOpacity: 0.4,
+    },
+  };
+
+  const config3 = {
+    data: {
+      type: "fetch",
+      value:
+        "https://render.alipay.com/p/yuyan/180020010001215413/antd-charts/column-bar-dodged.json",
+    },
+    xField: "state",
+    yField: "population",
+    colorField: "age",
+    group: true,
+    sort: {
+      reverse: true,
+      by: "y",
+    },
+    axis: {
+      y: { labelFormatter: "~s" },
+    },
+    interaction: {
+      tooltip: { shared: true },
+      elementHighlightByColor: { background: true },
+    },
+  } as any;
+
   return (
     <div className="p-2">
-      <Box h={700}>
-        <Area {...config} />
-      </Box>
+      <Grid columns={24}>
+        <Grid.Col span={12}>
+          <Box h={700}>
+            <Area {...config} />
+          </Box>
+        </Grid.Col>
+        <Grid.Col span={12}>
+          <Box h={700}>
+            <Column {...config3} />
+          </Box>
+        </Grid.Col>
+      </Grid>
+
       <NavButton to="/about/me">About Me</NavButton>
       <Outlet />
     </div>
