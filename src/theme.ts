72abcb27c4928103549208e52cf49e31e49acc77
ZB <zachary@cleancult.com>
2024-03-28 23:42:08 -0400

    added

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,17 @@
 import { createTheme } from "@mantine/core";
-
 export const theme = createTheme({
-  /* Put your mantine theme override here */
+  fontFamily: "Inter",
+  fontFamilyMonospace: "Inter",
+  headings: {
+    fontFamily: "Inter",
+
+    sizes: {
+      h1: {},
+      h2: {},
+      h3: {},
+      h4: {},
+      h5: {},
+      h6: {},
+    },
+  },
 });
